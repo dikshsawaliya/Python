1ee6a4a9bab4c64435be3661fb485886ee9635ee
Diksh Sawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2022-01-13 15:58:13 +0530

    Starting the Repo Guess Game

---
+++ Guess_Game.py
@@ -0,0 +1,9 @@
+right_answer = 7
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess_number = int(input("Guess the number: "))
+    guess_count +=1
+    if guess_number == right_answer:
+        print("You Guessed it right !")
+        break
---
+++ hello.py
@@ -0,0 +1,14 @@
+good_credit = False
+bad_credit = True
+
+if  good_credit:
+    print("Down payment 10%")
+    final_payment = 100 + 10 / 100
+    print(final_payment)
+else:
+    print("down payment 20%")
+    final_payment = 100 + 20/100
+    print(final_payment)
+
+
+
